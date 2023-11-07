ec314930364d350f60d502cf554b6e4bf37188b2
Daniel Mora <danielmora@Daniels-MacBook-Air.local>
2023-11-07 16:02:55 -0700

    styling css: jsx css

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from '@components/Navbar/Navbar'
-import styles from './layout.module.css'
+
 const Layout: React.FC = ({ children }) => {
   return (
-    <div className={styles.container}>
+    <div>
       <Navbar />
       {children}
+
       <footer>This is the footer</footer>
+
+      <style jsx>{`
+        footer {
+          background: aqua;
+        }
+      `}</style>
     </div>
   )
 }
