611d8febc19c101100bb4c0253eb0fe3e738b571
Daniel Mora <danielmora@Daniels-MacBook-Air.local>
2023-11-07 15:42:19 -0700

    styleing: global.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout/Layout'
+import Layout from '@components/Layout/Layout'
+
+import '../global.css'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
