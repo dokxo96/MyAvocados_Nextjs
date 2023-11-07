52af270e7dd3334b2ae4b3e5f4b91cbdccb5dbf6
Daniel Mora <danielmora@Daniels-MacBook-Air.local>
2023-11-07 12:54:43 -0700

    first endpoint connected to our local DB

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,7 +1,12 @@
 import { IncomingMessage, ServerResponse } from 'http'
-
+import DB from '@database'
 const allAvos = async (req: IncomingMessage, res: ServerResponse) => {
-  res.end(JSON.stringify({ hello: 'world' }))
+  const db = new DB()
+  const allEntries = await db.getAll()
+  const length = allEntries.length
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({ data: allEntries, length }))
 }
 
 export default allAvos
