4d48eb60084ebef087feaef0e94c6388adc2a6b2
Daniel Mora <danielmora@Daniels-MacBook-Air.local>
2023-11-08 08:37:18 -0700

    learning Server side redenring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    'https://my-avocados-nextjs-31mli2755-dokxo96.vercel.app/api/avo'
+  )
+  console.log(res)
+  const { data: productList }: TAPIAvoResponse = await res.json()
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
-
+  return {
+    props: {
+      productList,
+    },
+  }
+}
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
